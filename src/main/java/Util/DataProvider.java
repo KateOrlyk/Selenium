1cdf64130292d31a4df206819a7f2d84f9823597
KateOrlyk <kate.suprunenko24@gmail.com>
2022-09-14 12:52:29 +0300

    Метод RandomName и RandomAge

--- src/main/java/Util/DataProvider.java
+++ src/main/java/Util/DataProvider.java
@@ -6,13 +6,13 @@ import java.util.Random;
 
 public class DataProvider {
     static List<String> listname = Arrays.asList("Kate", "Pasha", "Arthur");
-    static List<Integer> integerList = Arrays.asList(18, 24, 27);
+    static List<String> listage = Arrays.asList("18", "24", "27");
 
     public static String getRandomName() {
         return listname.get(new Random().nextInt(listname.size()));
     }
 
-    public static Integer getRandomAge() {
-        return integerList.get(new Random().nextInt(integerList.size()));
+    public static CharSequence getRandomAge() {
+        return listage.get(new Random().nextInt(listage.size()));
     }
 }
